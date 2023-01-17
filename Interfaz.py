f23f3058b51ebfc5fee559f15bf6b8a72079e23c
Fernando <fer_f@outlook.com>
2023-01-16 22:59:53 -0600

    Interfaz y comandos listos

--- Interfaz.py
+++ Interfaz.py
@@ -1,79 +1,49 @@
 import tkinter as tk
-
 from tkinter import *
-
 from tkinter import ttk
-
 from tkinter import messagebox
-
 from proyecto import *
 
-
-
-#Funciones
-
+#Funcion para mostrar el texto en interfaz
 def mostrarTexto(texto):
-
-    outputtext.delete('1.0', END)
-
-    outputtext.insert(INSERT,texto)
-
-
+    outputtext.delete('1.0', END) #Se borra el texto existente
+    outputtext.insert(INSERT,texto) #Se agrega el nuevo texto
 
 #Configuracion de la ventana
-
 ventana = Tk()
+ventana.title("Practica Final") #Titulo de ventana
+ventana.geometry("1000x500") #Tamanio de ventana
+ventana["bg"] = "#242424" #Color de fondo
 
-ventana.title("Practica Final")
+#Etiqueta con el titulo
+titulo = Label(text="Administracion de Servicios en Red\nPractica Final\n Cortes Lopez Jaime Alejandro / Martinez Martinez Fernando",
+bg="#242424", font=("Arial", 16), fg="#ffffff")
+titulo.place(x=300,y=2)
 
-ventana.geometry("1500x800")
-
-ventana["bg"] = "#242424"
-
-
-
-titulo = Label(text="Administracion de Servicios en Red\nPractica Final\n Cortes Lopez Jaime Alejandro / Martinez Martinez Fernando", bg="#242424", font=("Arial", 16), fg="#ffffff")
-
-titulo.place(x=550,y=2)
-
-
-
-confRouterButton = Button( text='Configuracion Routers', bg="#26c6da" , command=lambda: mostrarTexto(confRouter()), width=20, height=2)
-
+#Boton para obtener configuracion de interfaces
+confRouterButton = Button( text='Configuracion Routers', bg="#26c6da", 
+	command=lambda: mostrarTexto(executeCommand('show ip int brief')), width=20, height=2)
 confRouterButton.place(x=20, y=100)
 
+#Boton para obtener configuracion de ACL
+confACLButton = Button( text='Configuracion ACL', bg="#26c6da", 
+	command=lambda: mostrarTexto(executeCommand('show access-list')), width=20, height=2)
+confACLButton.place(x=20, y=150)
 
+#Boton para obtener configuracion de NAT
+confNATButton = Button( text='Configuracion NAT', bg="#26c6da", 
+	command=lambda: mostrarTexto(executeCommand('show ip nat translation')), width=20, height=2)
+confNATButton.place(x=20, y=200)
 
-confVPCButton = Button( text='Configuracion VPCs', bg="#26c6da" , command=lambda: mostrarTexto("VPC Config"), width=20, height=2)
+#Boton para obtener configuracion de enrutamiento
+confEnrutButton = Button( text='Mostrar enrutamiento', bg="#26c6da", 
+	command=lambda: mostrarTexto(executeCommand('show ip rou')), width=20, height=2)
+confEnrutButton.place(x=20, y=250)
 
-confVPCButton.place(x=20, y=150)
-
-
-
-confACLButton = Button( text='Configuracion ACL', bg="#26c6da" , command=lambda: mostrarTexto(confACL()), width=20, height=2)
-
-confACLButton.place(x=20, y=200)
-
-
-
-confNATButton = Button( text='Configuracion NAT', bg="#26c6da" , command=lambda: mostrarTexto(confNAT()), width=20, height=2)
-
-confNATButton.place(x=20, y=250)
-
-
-
-confEnrutButton = Button( text='Mostrar enrutamiento', bg="#26c6da" , command=lambda: mostrarTexto(confEnrut()), width=20, height=2)
-
-confEnrutButton.place(x=20, y=300)
-
-
-
-outputtext = Text(height=35, width=150)
-
+#Componente para mostrar texto en la interfaz
+outputtext = Text(height=20, width=90)
 outputtext.grid(column=1, row=5)
-
 outputtext.place(x=250, y=100)
 
-
-
+#Se inicia la interfaz
 ventana.mainloop()
--- Proyecto.py
+++ Proyecto.py
@@ -1,177 +1,32 @@
 import pexpect
 
-
-
+#Se guardan las etiquetas e ip's de cada router
 devices = {'R1': {'prompt': 'R1#', 'ip': '192.168.1.1'},
-
           	'R2': {'prompt': 'R2#', 'ip': '172.16.1.2'},
-
           	'R3': {'prompt': 'R3#', 'ip': '172.16.1.6'},
-
           	'R4': {'prompt': 'R4#', 'ip': '172.16.1.14'},
-
           	'R5': {'prompt': 'R5#', 'ip': '172.16.1.18'},
-
           	'R6': {'prompt': 'R6#', 'ip': '172.16.0.2'},}
 
-username = 'cisco'
-
-password = 'cisco'
-
-
-
-
-
-def confRouter():
-
-	text = ""
-
-	for device in devices.keys():
-
-		device_prompt = devices[device]['prompt']
-
-		child = pexpect.spawn('telnet ' + devices[device]['ip'])
-
-		child.expect('Username:')
-
-		child.sendline(username)
-
-		child.expect('Password:')
-
-		child.sendline(password)
-
-		child.expect(device_prompt)
-
-		text+=device_prompt+"\n"
-
-		child.sendline('terminal length 0')  
-
-		child.expect(device_prompt) 
-
-		child.sendline('show ip int brief')
-
-		child.expect(device_prompt)
-
-		text+= str(child.before, "UTF-8")+"\n\n"
-
-		child.sendline('exit')
-
-	return text
-
-	
-
-def confACL():
-
-	text = ""
-
-	for device in devices.keys():
-
-		device_prompt = devices[device]['prompt']
-
-		child = pexpect.spawn('telnet ' + devices[device]['ip'])
-
-		child.expect('Username:')
-
-		child.sendline(username)
-
-		child.expect('Password:')
-
-		child.sendline(password)
-
-		child.expect(device_prompt)
-
-		text+=device_prompt+"\n"
-
-		child.sendline('terminal length 0')  
-
-		child.expect(device_prompt) 
-
-		child.sendline('show access-list')
-
-		child.expect(device_prompt)
-
-		text+= str(child.before, "UTF-8")+"\n\n"
-
-		child.sendline('exit')
-
-	return text
-
-	
-
-def confNAT():
-
-	text = ""
-
-	for device in devices.keys():
-
-		device_prompt = devices[device]['prompt']
-
-		child = pexpect.spawn('telnet ' + devices[device]['ip'])
-
-		child.expect('Username:')
-
-		child.sendline(username)
-
-		child.expect('Password:')
-
-		child.sendline(password)
-
-		child.expect(device_prompt)
-
-		text+=device_prompt+"\n"
-
-		child.sendline('terminal length 0')  
-
-		child.expect(device_prompt) 
-
-		child.sendline('show ip nat translation')
-
-		child.expect(device_prompt)
-
-		text+= str(child.before, "UTF-8")+"\n\n"
-
-		child.sendline('exit')
-
-	return text
-
-	
-
-	
-
-def confEnrut():
-
-	text = ""
-
-	for device in devices.keys():
-
-		device_prompt = devices[device]['prompt']
-
-		child = pexpect.spawn('telnet ' + devices[device]['ip'])
-
-		child.expect('Username:')
-
-		child.sendline(username)
-
-		child.expect('Password:')
-
-		child.sendline(password)
-
-		child.expect(device_prompt)
-
-		text+=device_prompt+"\n"
-
-		child.sendline('terminal length 0')  
-
-		child.expect(device_prompt) 
-
-		child.sendline('show ip rou')
-
-		child.expect(device_prompt)
-
-		text+= str(child.before, "UTF-8")+"\n\n"
-
-		child.sendline('exit')
-
-	return text	
-
-	
+username = 'cisco' #Usuario
+password = 'cisco' #Contrasenia
+
+#Fucion para ejecutar un comando a traves de Telnet
+def executeCommand(command): #Recibe el comando (string) como parametro
+	text = "" #Variable que contiene la respuesta del comando
+	for device in devices.keys(): #Se itera cada router
+		device_prompt = devices[device]['prompt'] #Se guarda la etiqueta de cada router
+		child = pexpect.spawn('telnet ' + devices[device]['ip']) #Se hace la conexion por Telnet
+		child.expect('Username:') #Se espera el usuario
+		child.sendline(username) #Se ingresa el usuario
+		child.expect('Password:') #Se espera por la contrasenia
+		child.sendline(password) #Se ingresa la contrasenia
+		child.expect(device_prompt) #Se espera por la etiqueta del router
+		text+=device_prompt+"\n" #Se guarda la etiqueta del router en la respuesta
+		child.sendline('terminal length 0') #Se elimina la opcion --more-- al ejecutar un comando largo
+		child.expect(device_prompt) #Se espera por la etiqueta del router
+		child.sendline(command) #Se envia el comando
+		child.expect(device_prompt) #Se espera por la etiqueta del router
+		text+= str(child.before, "UTF-8")+"\n\n" #Se obtiene la respuesta y se convierte a string
+		child.sendline('exit') #Se cierra la sesion de telnet
+	return text #Se devuelve toda la respuesta obtenida de todos los routers
